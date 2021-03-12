ab7ad64810d7786dfbc4d4be4f5601a9400b8036
koyuda <55407066+yanok13@users.noreply.github.com>
2021-03-12 21:18:18 +0200

    creating trainig components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <user-profile :user="user" @save-user="user = $event"></user-profile>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import Profile from "@/components/User/Profile";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        HelloWorld, 'user-profile': Profile
+    },
+    data() {
+        return {
+            user: {
+                name: 'John',
+                surname: 'Doe'
+            },
+            testData: 'Data From App'
+        }
+    },
+    provide() {
+        return {
+            testData: this.testData
+        }
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
---
+++ src/components/User/Profile.vue
@@ -0,0 +1,58 @@
+<template>
+    <h1>User Profile</h1>
+    <p><strong>Name</strong> {{ user.name }}</p>
+    <p><strong>Surname</strong> {{ user.surname }}</p>
+
+    <div v-if="!showForm">
+        <button @click="showForm = !showForm">Edit</button>
+    </div>
+    <div v-else>
+        <profile-form :user="user" @save-user="saveUser" @cancel="showForm = false"></profile-form>
+    </div>
+</template>
+
+<script>
+import ProfileForm from "@/components/User/ProfileForm";
+export default {
+    name: "Profile",
+    components: {ProfileForm},
+    props: {
+        // user: Object
+        user: {
+            type: Object,
+            required: true
+        }
+        /*propD: {
+          type: Number,
+          default: 100,
+        },
+        propE: {
+          type: Object,
+          // Object or array defaults must be returned from
+          // a factory function
+          default: function() {
+            return { message: 'hello' }
+          }
+        },
+        propF: {
+          validator: function(value) {
+            // The value must match one of these strings
+            return ['success', 'warning', 'danger'].indexOf(value) !== -1
+          }
+        }*/
+    },
+    data() {
+        return {
+            showForm: false
+        }
+    },
+    methods: {
+        saveUser(user) {
+            this.$emit('save-user', user)
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
